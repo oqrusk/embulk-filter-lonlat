74eda7f6bc71064d718d9e7a3aa5e62fc8add575
oqrusk <oqrusk@gmail.com>
2016-11-24 00:15:57 +0900

    can convert degree to millisec

--- src/main/java/org/embulk/filter/lonlat/LonlatFilterPlugin.java
+++ src/main/java/org/embulk/filter/lonlat/LonlatFilterPlugin.java
@@ -1,46 +1,276 @@
 package org.embulk.filter.lonlat;
 
 import com.google.common.base.Optional;
-import org.embulk.config.*;
+import com.google.common.collect.ImmutableList;
+import org.embulk.config.Config;
+import org.embulk.config.ConfigDefault;
+import org.embulk.config.ConfigSource;
+import org.embulk.config.Task;
+import org.embulk.config.TaskSource;
+import org.embulk.spi.Column;
+import org.embulk.spi.ColumnVisitor;
+import org.embulk.spi.Exec;
 import org.embulk.spi.FilterPlugin;
+import org.embulk.spi.Page;
+import org.embulk.spi.PageBuilder;
 import org.embulk.spi.PageOutput;
+import org.embulk.spi.PageReader;
 import org.embulk.spi.Schema;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.embulk.spi.type.Types.DOUBLE;
+import static org.embulk.spi.type.Types.LONG;
+
 public class LonlatFilterPlugin
-        implements FilterPlugin {
+        implements FilterPlugin
+{
+    public static final String TYPE_DEGREE = "DEG";
+    public static final String TYPE_MILLISEC = "MSEC";
+
+    static Schema buildOutputSchema(PluginTask task, Schema inputSchema)
+    {
+        List<LonLatColumnTask> latColumns = task.getLatColumns();
+        List<LonLatColumnTask> lonColumns = task.getLonColumns();
+
+        // Automatically get column type from inputSchema for columns and dropColumns
+        ImmutableList.Builder<Column> builder = ImmutableList.builder();
+        int i = 0;
+
+        for (Column column : inputSchema.getColumns()) {
+
+            Column outputColumn = new Column(i, column.getName(), column.getType());
+
+            for (LonLatColumnTask latColumn : latColumns) {
+                if (!column.getName().equals(latColumn.getName())) {
+                    continue;
+                }
+                outputColumn = (latColumn.getFormat().equals(TYPE_DEGREE)) ?
+                        new Column(i, column.getName(), DOUBLE) : new Column(i, column.getName(), LONG);
+            }
+            for (LonLatColumnTask lonColumn : lonColumns) {
+                if (!column.getName().equals(lonColumn.getName())) {
+                    continue;
+                }
+                outputColumn = (lonColumn.getFormat().equals(TYPE_DEGREE)) ?
+                        new Column(i, column.getName(), DOUBLE) : new Column(i, column.getName(), LONG);
+            }
+            builder.add(outputColumn);
+            i++;
+        }
+        return new Schema(builder.build());
+    }
+
+    static Map<String, LonLatColumnTask> getTaskColumnMap(
+            List<LonLatColumnTask> latColumnTasks,
+            List<LonLatColumnTask> lonColumnTasks)
+    {
+        Map<String, LonLatColumnTask> m = new HashMap<>();
+        for (LonLatColumnTask columnTask : latColumnTasks) {
+            m.put(columnTask.getName(), columnTask);
+        }
+        for (LonLatColumnTask columnTask : lonColumnTasks) {
+            m.put(columnTask.getName(), columnTask);
+        }
+        return m;
+    }
+
     @Override
     public void transaction(ConfigSource config, Schema inputSchema,
-                            FilterPlugin.Control control) {
+            FilterPlugin.Control control)
+    {
         PluginTask task = config.loadConfig(PluginTask.class);
-
-        Schema outputSchema = inputSchema;
-
+        for (LonLatColumnTask latColumnTask : task.getLatColumns()) {
+            // throws exception if the column name does not exist
+            Column column = inputSchema.lookupColumn(latColumnTask.getName());
+        }
+        for (LonLatColumnTask lonColumnTask : task.getLonColumns()) {
+            // throws exception if the column name does not exist
+            inputSchema.lookupColumn(lonColumnTask.getName());
+        }
+//        Schema outputSchema = inputSchema;
+        Schema outputSchema = buildOutputSchema(task, inputSchema);
         control.run(task.dump(), outputSchema);
     }
 
+    private long convertDeg2Dms(double deg)
+    {
+        Double d = (deg * 60 * 60 * 1000);
+        return d.longValue();
+    }
+
+    private double convertMsec2Deg(long mSec)
+    {
+        return mSec / (60 * 60 * 1000);
+    }
+
     @Override
-    public PageOutput open(TaskSource taskSource, Schema inputSchema,
-                           Schema outputSchema, PageOutput output) {
+    public PageOutput open(TaskSource taskSource, final Schema inputSchema,
+            final Schema outputSchema, final PageOutput output)
+    {
         PluginTask task = taskSource.loadTask(PluginTask.class);
 
-        // Write your code here :)
-        throw new UnsupportedOperationException("LonlatFilterPlugin.open method is not implemented yet");
+        final Map<String, LonLatColumnTask> columnTaskMap = getTaskColumnMap(task.getLatColumns(), task.getLonColumns());
+
+//        throw new UnsupportedOperationException("LonlatFilterPlugin.open method is not implemented yet");
+        return new PageOutput()
+        {
+            private PageReader reader = new PageReader(inputSchema);
+            private PageBuilder builder = new PageBuilder(
+                    Exec.getBufferAllocator(), outputSchema, output);
+            private ColumnVisitorImpl visitor = new ColumnVisitorImpl(builder);
+
+            @Override
+            public void add(Page page)
+            {
+                reader.setPage(page);
+
+                while (reader.nextRecord()) {
+                    for (Column column : inputSchema.getColumns()) {
+                        String colName = column.getName();
+                        LonLatColumnTask colTask = columnTaskMap.get(colName);
+
+                        // do nothing
+                        if (colTask == null) {
+                            column.visit(visitor);
+                            continue;
+                        }
+
+                        // do convert
+                        if (colTask.getFormat().or("").equals(TYPE_MILLISEC)) {
+                            double raw = reader.getDouble(column);
+                            builder.setLong(column, convertDeg2Dms(raw));
+                        }
+                        else if (colTask.getFormat().or("").equals(TYPE_DEGREE)) {
+                            long raw = reader.getLong(column);
+                            builder.setDouble(column, convertMsec2Deg(raw));
+                        }
+                    }
+                    //visitcolumns
+                    builder.addRecord();
+                }
+            }
+
+            @Override
+            public void finish()
+            {
+                builder.finish();
+            }
+
+            @Override
+            public void close()
+            {
+                builder.close();
+            }
+
+            class ColumnVisitorImpl
+                    implements ColumnVisitor
+            {
+                private final PageBuilder builder;
+
+                ColumnVisitorImpl(PageBuilder builder)
+                {
+                    this.builder = builder;
+                }
+
+                @Override
+                public void booleanColumn(Column outputColumn)
+                {
+                    if (reader.isNull(outputColumn)) {
+                        builder.setNull(outputColumn);
+                    }
+                    else {
+                        builder.setBoolean(outputColumn, reader.getBoolean(outputColumn));
+                    }
+                }
+
+                @Override
+                public void longColumn(Column outputColumn)
+                {
+                    if (reader.isNull(outputColumn)) {
+                        builder.setNull(outputColumn);
+                    }
+                    else {
+                        builder.setLong(outputColumn, reader.getLong(outputColumn));
+                    }
+                }
+
+                @Override
+                public void doubleColumn(Column outputColumn)
+                {
+                    if (reader.isNull(outputColumn)) {
+                        builder.setNull(outputColumn);
+                    }
+                    else {
+                        builder.setDouble(outputColumn, reader.getDouble(outputColumn));
+                    }
+                }
+
+                @Override
+                public void stringColumn(Column outputColumn)
+                {
+                    if (reader.isNull(outputColumn)) {
+                        builder.setNull(outputColumn);
+                    }
+                    else {
+                        builder.setString(outputColumn, reader.getString(outputColumn));
+                    }
+                }
+
+                @Override
+                public void timestampColumn(Column outputColumn)
+                {
+                    if (reader.isNull(outputColumn)) {
+                        builder.setNull(outputColumn);
+                    }
+                    else {
+                        builder.setTimestamp(outputColumn, reader.getTimestamp(outputColumn));
+                    }
+                }
+
+                @Override
+                public void jsonColumn(Column outputColumn)
+                {
+                    if (reader.isNull(outputColumn)) {
+                        builder.setNull(outputColumn);
+                    }
+                    else {
+                        builder.setJson(outputColumn, reader.getJson(outputColumn));
+                    }
+                }
+            }
+        };
     }
 
     public interface PluginTask
-            extends Task {
-        // configuration option 1 (required integer)
-        @Config("option1")
-        public int getOption1();
+            extends Task
+    {
 
-        // configuration option 2 (optional string, null is not allowed)
-        @Config("option2")
-        @ConfigDefault("\"myvalue\"")
-        public String getOption2();
+        @Config("lon_columns")
+        public List<LonLatColumnTask> getLonColumns();
 
-        // configuration option 3 (optional string, null is allowed)
-        @Config("option3")
-        @ConfigDefault("null")
-        public Optional<String> getOption3();
+        @Config("lat_columns")
+        public List<LonLatColumnTask> getLatColumns();
+    }
+
+    public interface LonLatColumnTask
+            extends Task
+    {
+        //        {name: lat, type: lat, format: DMS, datum: WGS84}
+        @Config("name")
+        String getName();
+
+        @Config("type")
+        String getType();
+
+        @Config("format")
+        @ConfigDefault("DEG")
+        Optional<String> getFormat();
+
+        @Config("datum")
+        @ConfigDefault("WGS84")
+        Optional<String> getDatum();
     }
 }
--- src/test/java/org/embulk/filter/lonlat/TestLonlatFilterPlugin.java
+++ src/test/java/org/embulk/filter/lonlat/TestLonlatFilterPlugin.java
@@ -1,4 +1,185 @@
 package org.embulk.filter.lonlat;
 
-public class TestLonlatFilterPlugin {
+import com.google.common.collect.Lists;
+import org.embulk.EmbulkTestRuntime;
+import org.embulk.config.ConfigException;
+import org.embulk.config.ConfigLoader;
+import org.embulk.config.ConfigSource;
+import org.embulk.config.TaskSource;
+import org.embulk.filter.lonlat.LonlatFilterPlugin.PluginTask;
+import org.embulk.spi.Column;
+import org.embulk.spi.Exec;
+import org.embulk.spi.FilterPlugin;
+import org.embulk.spi.Page;
+import org.embulk.spi.PageOutput;
+import org.embulk.spi.PageReader;
+import org.embulk.spi.PageTestUtils;
+import org.embulk.spi.Schema;
+import org.embulk.spi.TestPageBuilderReader;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static org.embulk.spi.type.Types.DOUBLE;
+import static org.embulk.spi.type.Types.STRING;
+import static org.junit.Assert.assertEquals;
+
+public class TestLonlatFilterPlugin
+{
+    @Rule
+    public EmbulkTestRuntime runtime = new EmbulkTestRuntime();
+
+    private LonlatFilterPlugin plugin;
+
+    @Before
+    public void createResource()
+    {
+        plugin = new LonlatFilterPlugin();
+    }
+
+    private Schema schema(Column... columns)
+    {
+        return new Schema(Lists.newArrayList(columns));
+    }
+
+    private ConfigSource configFromYamlString(String... lines)
+    {
+        StringBuilder builder = new StringBuilder();
+        for (String line : lines) {
+            builder.append(line).append("\n");
+        }
+        String yamlString = builder.toString();
+
+        ConfigLoader loader = new ConfigLoader(Exec.getModelManager());
+        return loader.fromYamlString(yamlString);
+    }
+
+    private PluginTask taskFromYamlString(String... lines)
+    {
+        ConfigSource config = configFromYamlString(lines);
+        return config.loadConfig(PluginTask.class);
+    }
+
+    private void transaction(ConfigSource config, Schema inputSchema)
+    {
+        plugin.transaction(config, inputSchema, new FilterPlugin.Control()
+        {
+            @Override
+            public void run(TaskSource taskSource, Schema outputSchema)
+            {
+            }
+        });
+    }
+
+    @Test
+    public void test_configure_columns_option()
+    {
+        PluginTask task = taskFromYamlString(
+                "type: lonlat",
+                "lat_columns:",
+                "  - {name: lat, type: lat, format: MSEC, datum: WGS84}",
+                "lon_columns:",
+                "  - {name: lon, type: lon, format: MSEC, datum: WGS84}");
+
+        assertEquals(1, task.getLatColumns().size());
+        assertEquals(1, task.getLonColumns().size());
+    }
+
+    @Test
+    public void buildOutputSchema_DropColumns()
+    {
+        ConfigSource config = configFromYamlString(
+                "type: lonlat",
+                "lat_columns:",
+                "  - {name: lat, type: lat, format: MSEC, datum: WGS84}",
+                "lon_columns:",
+                "  - {name: lon, type: lon, format: MSEC, datum: WGS84}");
+
+        final Schema inputSchema = Schema.builder()
+                .add("lat", DOUBLE)
+                .add("lon", DOUBLE)
+                .build();
+
+        plugin = new LonlatFilterPlugin();
+        plugin.transaction(config, inputSchema, new FilterPlugin.Control()
+        {
+            @Override
+            public void run(TaskSource taskSource, Schema outputSchema)
+            {
+                TestPageBuilderReader.MockPageOutput mockPageOutput = new TestPageBuilderReader.MockPageOutput();
+                PageOutput pageOutput = plugin.open(taskSource,
+                        inputSchema,
+                        outputSchema,
+                        mockPageOutput);
+
+                double testLat = 35.6721277;
+                double testLon = 139.75891209999997;
+                for (Page page : PageTestUtils.buildPage(runtime.getBufferAllocator(),
+                        inputSchema, testLat, "lat")) {
+                    pageOutput.add(page);
+                }
+                for (Page page : PageTestUtils.buildPage(runtime.getBufferAllocator(),
+                        inputSchema, testLon, "lon")) {
+                    pageOutput.add(page);
+                }
+
+                pageOutput.finish();
+                pageOutput.close();
+
+                PageReader pageReader = new PageReader(outputSchema);
+
+                for (Page page : mockPageOutput.pages) {
+                    pageReader.setPage(page);
+                    assertEquals(128419659, pageReader.getLong(outputSchema.getColumn(0)));
+                    assertEquals(128419659, pageReader.getLong(outputSchema.getColumn(1)));
+                }
+            }
+        });
+    }
+
+    @Test
+    public void buildOutputSchema_AddColumns()
+    {
+        PluginTask task = taskFromYamlString(
+                "type: column",
+                "add_columns:",
+                "  - {name: timestamp, type: timestamp, default: 2015-07-13, format: \"%Y-%m-%d\", timezone: UTC}",
+                "  - {name: string, type: string, default: string}",
+                "  - {name: boolean, type: boolean, default: true}",
+                "  - {name: long, type: long, default: 0}",
+                "  - {name: double, type: double, default: 0.5}",
+                "  - {name: json, type: json, default: \"{\\\"foo\\\":\\\"bar\\\"}\" }");
+        Schema inputSchema = Schema.builder()
+                .add("keep_me", STRING)
+                .build();
+
+        Schema outputSchema = inputSchema;//ColumnFilterPlugin.buildOutputSchema(task, inputSchema);
+        assertEquals(7, outputSchema.size());
+
+        Column column;
+        {
+            column = outputSchema.getColumn(0);
+            assertEquals("keep_me", column.getName());
+        }
+        {
+            column = outputSchema.getColumn(1);
+            assertEquals("timestamp", column.getName());
+        }
+    }
+
+    @Test(expected = ConfigException.class)
+    public void configure_EitherOfColumnsOrDropColumnsCanBeSpecified()
+    {
+        ConfigSource config = configFromYamlString(
+                "type: column",
+                "columns:",
+                "- {name: a}",
+                "drop_columns:",
+                "- {name: a}");
+        Schema inputSchema = schema(
+                new Column(0, "a", STRING),
+                new Column(1, "b", STRING));
+
+        transaction(config, inputSchema);
+    }
 }
